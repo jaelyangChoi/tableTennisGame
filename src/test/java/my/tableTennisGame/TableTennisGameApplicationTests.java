c2b9d87cd2d696191f0fcf187a2a76a6510c17d5
Jealyang <jaelyangchoi@gmail.com>
2025-02-14 19:35:12 +0900

    Initial commit

---
+++ src/test/java/my/tableTennisGame/TableTennisGameApplicationTests.java
@@ -0,0 +1,13 @@
+package my.tableTennisGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableTennisGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
