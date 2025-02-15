76f2126ca726f7bf131aad92b47db3380a6bc160
Jealyang <jaelyangchoi@gmail.com>
2025-02-15 15:03:38 +0900

    test: "헬스 체크 테스트"
    단위 테스트 시 JPA 관련 빈 로딩하지 않기 위해 main 메서드의 @EnableJpaAuditing 분리

---
+++ src/test/java/my/tableTennisGame/web/SystemControllerTest.java
@@ -0,0 +1,31 @@
+package my.tableTennisGame.web;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(SystemController.class)
+class SystemControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @DisplayName("헬스 체크 성공 응답 테스트")
+    @Test
+    void healthCheck_Success() throws Exception {
+        mockMvc.perform(get("/health")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value(200))
+                .andExpect(jsonPath("$.message").value("API 요청이 성공했습니다."))
+                .andExpect(jsonPath("$.result").doesNotExist());
+    }
+
+}
