d07c8ec704bb334990362dea1adeafef5f2d682d
Jealyang <jaelyangchoi@gmail.com>
2025-02-15 18:57:15 +0900

    test: "초기화 API 컨트롤러 테스트"

--- src/test/java/my/tableTennisGame/web/SystemControllerTest.java
+++ src/test/java/my/tableTennisGame/web/SystemControllerTest.java
@@ -1,7 +1,9 @@
 package my.tableTennisGame.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import my.tableTennisGame.service.init.ExternalApiService;
 import my.tableTennisGame.service.init.InitService;
+import my.tableTennisGame.web.dto.init.InitReqDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,7 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -26,11 +29,32 @@ class SystemControllerTest {
     @MockitoBean
     private ExternalApiService externalApiService;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @DisplayName("헬스 체크 성공 응답 테스트")
     @Test
     void healthCheck_Success() throws Exception {
         mockMvc.perform(get("/health")
                         .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value(200))
+                .andExpect(jsonPath("$.message").value("API 요청이 성공했습니다."))
+                .andExpect(jsonPath("$.result").doesNotExist());
+    }
+
+    @DisplayName("초기화 성공 응답 테스트")
+    void initSuccess() throws Exception {
+        //given
+        InitReqDto initReqDto = new InitReqDto();
+        initReqDto.setSeed("30");
+        initReqDto.setQuantity("40");
+
+
+        //when & then
+        mockMvc.perform(post("/init")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(initReqDto)))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.code").value(200))
                 .andExpect(jsonPath("$.message").value("API 요청이 성공했습니다."))
