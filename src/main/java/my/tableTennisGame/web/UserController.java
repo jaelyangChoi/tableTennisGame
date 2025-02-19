e12c6588bb4fcfe9da2ffc5ce0bfcfc24cf4a971
Jealyang <jaelyangchoi@gmail.com>
2025-02-19 19:22:52 +0900

    feat: "유저 전체 조회 API 구현"

---
+++ src/main/java/my/tableTennisGame/web/UserController.java
@@ -0,0 +1,31 @@
+package my.tableTennisGame.web;
+
+import lombok.RequiredArgsConstructor;
+import my.tableTennisGame.service.user.UserService;
+import my.tableTennisGame.web.dto.ApiResponse;
+import my.tableTennisGame.web.dto.user.UserRespDto.UserListRespDto;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    /**
+     * 3. 유저 전체 조회 API
+     * - 페이징 처리를 위한 size, page 값을 RequestParameter로 받음
+     * - 모든 회원 정보를 응답(id 기준 오름차순으로 정렬해서 반환)
+     */
+    @GetMapping("/user")
+    public ApiResponse<UserListRespDto> findUserList(@PageableDefault(sort = "id", direction = Sort.Direction.ASC) Pageable pageable) {
+
+        UserListRespDto userListRespDto = userService.findUserList(pageable);
+
+        return ApiResponse.success(userListRespDto);
+    }
+}
---
+++ src/main/java/my/tableTennisGame/web/dto/user/UserRespDto.java
@@ -0,0 +1,50 @@
+package my.tableTennisGame.web.dto.user;
+
+import lombok.Getter;
+import lombok.Setter;
+import my.tableTennisGame.domain.user.User;
+import org.springframework.data.domain.Page;
+
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserRespDto {
+
+    @Getter
+    @Setter
+    public static class UserListRespDto {
+        private int totalElements;
+        private int totalPages;
+        private List<UserDto> userList = new ArrayList<>();
+
+        public UserListRespDto(Page<User> userPage) {
+            this.totalElements = (int) userPage.getTotalElements();
+            this.totalPages = (int) userPage.getTotalPages();
+            this.userList = userPage.getContent().stream()
+                    .map(UserDto::new).toList();
+        }
+
+        @Getter
+        @Setter
+        public static class UserDto {
+            private int id;
+            private int fakerId;
+            private String name;
+            private String email;
+            private String status;
+            private String cratedAt;
+            private String updatedAt;
+
+            public UserDto(User user) {
+                this.id = user.getId();
+                this.fakerId = user.getFakerId();
+                this.name = user.getName();
+                this.email = user.getEmail();
+                this.status = user.getStatus().name();
+                this.cratedAt = user.getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+                this.updatedAt = user.getUpdatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+            }
+        }
+    }
+}
