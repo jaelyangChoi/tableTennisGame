66c4491dffae27889c2d2d255007f9c391e7aeb4
Jealyang <jaelyangchoi@gmail.com>
2025-02-20 22:01:28 +0900

    test: "방에 참여한 유저 매핑 엔티티 생성 테스트"

---
+++ src/test/java/my/tableTennisGame/service/UserRoomServiceTest.java
@@ -0,0 +1,50 @@
+package my.tableTennisGame.service;
+
+import my.tableTennisGame.domain.room.Room;
+import my.tableTennisGame.domain.user.User;
+import my.tableTennisGame.domain.userRoom.UserRoom;
+import my.tableTennisGame.dummy.DummyObject;
+import my.tableTennisGame.repository.UserRoomRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UserRoomServiceTest extends DummyObject {
+
+    @Mock
+    private UserRoomRepository userRoomRepository;
+
+    @InjectMocks
+    private UserRoomService userRoomService;
+
+    @DisplayName("방에 참여한 유저 매핑 엔티티 생성 테스트")
+    @Test
+    void addUserToRoom() {
+        // given
+        int userId = 1;
+        String roomType = "SINGLE";
+        User mockHost = newMockUser(userId, "jaeryang");
+        Room mockRoom = newMockRoom(1, mockHost, roomType);
+        when(userRoomRepository.save(any())).thenAnswer(invocationOnMock -> invocationOnMock.getArgument(0));
+
+        // when
+        UserRoom userRoom = userRoomService.addUserToRoom(mockHost, mockRoom);
+
+        // then
+        assertNotNull(userRoom);
+        assertEquals(mockHost, userRoom.getUser());
+        assertEquals(userId, userRoom.getUser().getId());
+        assertEquals(mockRoom, userRoom.getRoom());
+        assertEquals(roomType, userRoom.getRoom().getRoomType().name());
+    }
+
+}
